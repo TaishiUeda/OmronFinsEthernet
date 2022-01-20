f6db1b01c412340a14b93f30069664aab6cce1cb
Roms in Kojiya Ubuntu <git-roms@nosuchanaddress.com>
2022-01-21 01:02:51 +0900

    modified to devide data creator and udp

--- src/omronfins/datacreator.py
+++ src/omronfins/datacreator.py
@@ -9,7 +9,6 @@
 """
 import struct
 import binascii
-import socket
 
 
 class DataCreator():
@@ -29,8 +28,12 @@ class DataCreator():
     STR = 's'
     BYTES = 's'
 
-    def __init__(self, src_node_num=0, src_unit_addr=0, src_net_addr=0, srv_id=0):
+    def __init__(self, src_net_addr=0, src_node_num=0, src_unit_addr=0, srv_id=0):
         """!Initializer
+        @param[in] src_net_addr Source network address.
+            Specify within the following ranges.
+            - 0x00: Local netwotk
+            - 0x01 to 0x7F: Remote netwotk (1 to 127 decimal)
         @param[in] src_node_num Source node number.
             Specify within the following ranges.
             - 0x01 to 0x7E: Node number in SYSMAC NET network (1 to 126 decimal)
@@ -42,10 +45,6 @@ class DataCreator():
             - 0xFE: SYSMAC NET Link Unit or SYSMAC LINK Unit connected to network
             - 0x10 to 0x1F: CPU Bus Unit (10 + unit number in hexadecimal
             Note: The unit address for a CPU Bus Unit is 10 (hexadecimal) plus the unit number set on the front panel of the CPU Bus Unit.
-        @param[in] src_net_addr Source network address.
-            Specify within the following ranges.
-            - 0x00: Local netwotk
-            - 0x01 to 0x7F: Remote netwotk (1 to 127 decimal)
         @param[in] src_id Service ID. Used to identify the processing generating the transmission.
             Set the SID to any number between 0x00 and 0xFF
         """
@@ -55,33 +54,15 @@ class DataCreator():
         self._srv_id = srv_id
         self._header_bin = None
         self.set_destination(0, 0)
-        self._sock = None
-        self._ip_addr = 'localhost'
-        self._port = 9600
 
-    def close(self):
-        if self._sock is not None:
-            self._sock.close()
-
-    def open(self, ip_addr, port):
-        self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self._ip_addr = ip_addr
-        self._port = port
-        try:
-            self._sock.settimeout(5)
-            self._sock.connect((self._ip_addr, self._port))
-        except socket.timeout:
-            return -1
-        except socket.error:
-            return -2
-        except BaseException:
-            return -3
-        return 0
-
-    def set_destination(self, dst_node_num, dst_unit_addr, dst_net_addr=0, delay=2):
+    def set_destination(self, dst_net_addr, dst_node_num, dst_unit_addr=0, delay=2):
         """!
         Create header data for a message according to source information and
         destination information input as arguments.
+        @param[in] dst_net_addr Destination network address.
+            Specify within the following ranges.
+            - 0x00: Local netwotk
+            - 0x01 to 0x7F: Remote netwotk (1 to 127 decimal)
         @param[in] dst_node_num Destination node number.
             Specify within the following ranges.
             - 0x01 to 0x7E: Node number in SYSMAC NET network (1 to 126 decimal)
@@ -93,10 +74,6 @@ class DataCreator():
             - 0xFE: SYSMAC NET Link Unit or SYSMAC LINK Unit connected to network
             - 0x10 to 0x1F: CPU Bus Unit (10 + unit number in hexadecimal
             Note: The unit address for a CPU Bus Unit is 10 (hexadecimal) plus the unit number set on the front panel of the CPU Bus Unit.
-        @param[in] dst_net_addr Destination network address.
-            Specify within the following ranges.
-            - 0x00: Local netwotk
-            - 0x01 to 0x7F: Remote netwotk (1 to 127 decimal)
         @param[in] delay It is possible to specify the response delay of a PC in 10-ms increments.
             Selecting 0 through F in hexadecimal sets the time required for a PC to respond
             to the host computer after the PC receives a command block from the host computer.
@@ -105,31 +82,6 @@ class DataCreator():
         @return bytes encoded header
         """
         self._header_bin = self._create_header(dst_node_num, dst_unit_addr, dst_net_addr, delay)
-
-    def _send_and_recv(self, msg):
-        try:
-            self._sock.settimeout(1)
-            self._sock.sendall(msg)
-        except socket.timeout:
-            return -1, None
-        except socket.error:
-            return -2, None
-        try:
-            self._sock.settimeout(1)
-            res_data = self._sock.recv(4096)
-        except socket.timeout:
-            return -1, None
-        except socket.error:
-            return -2, None
-        return len(res_data), res_data
-
-    def exec_read_mem_area(self, mem_area, addr, bit, num):
-        msg = self.command_read_mem_area(mem_area, addr, bit, num)
-        return self._send_and_recv(msg)
-
-    def exec_write_mem_area(self, mem_area, addr, bit, num, data):
-        msg = self.command_write_mem_area(mem_area, addr, bit, num, data)
-        return self._send_and_recv(msg)
 
     def _convert_data2ascii(self, data):
         """!
@@ -141,8 +93,6 @@ class DataCreator():
                  (0, DataCreator.USHORT),
                  ('A', DataCreator.CHAR),
                  (b'Hoge', DataCreator.STR)]
-                will be converted to
-                b'0000005a0000005000004148Hoge'
         @return bytes
         """
         out_bin = b''
@@ -150,12 +100,12 @@ class DataCreator():
             if a_data[1] == DataCreator.BYTES:
                 out_bin += a_data[0]
             elif a_data[1] == DataCreator.STR:
-                out_bin += a_data[0].encode('ascii')
+                out_bin += a_data[0].encode('utf-8')
             else:
-                out_bin += struct.pack('!'+a_data[1][0], a_data[0])
+                out_bin += struct.pack('!'+a_data[1], a_data[0])
         return out_bin
 
-    def _convert_ascii2data(self, data, fmt):
+    def convert_ascii2data(self, data, fmt):
         """!
         convert ascii data according to format.
         @param data formatted data as a list contains tuples of (type, size).
@@ -207,15 +157,6 @@ class DataCreator():
         else:
             fmt.append((data[0], data[1]))
         return self._header_bin + self._convert_data2ascii(fmt)
-
-    def is_response_to_me(self, response):
-        """!
-        check if the response is to this node according to the header information.
-        @param[in] response responsed data as bytes
-        @retval True the response is to this node.
-        @retval False the response is to the other nodes.
-        """
-        return response[:20] == self._header_bin
 
     def __del__(self):
         """!Destructor
--- test/test.py
+++ test/test.py
@@ -9,10 +9,9 @@ import unittest
 import time
 import os
 import sys
-from fins import udp
-import fins
 sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
 from omronfins.datacreator import DataCreator
+from omronfins.finsudp import FinsUDP
 
 
 class TestCase0(unittest.TestCase):
@@ -21,15 +20,13 @@ class TestCase0(unittest.TestCase):
     ## Test cases
     - [x] test01: create header from destination info and source info.
     - [x] test02: create memory area read command
-    - [x] test03: check if the command code in response
-        is corespondance to sent message.
-    - [x] test04: convert text data in resnpose msg
-    - [ ] test05: create command to write mem area
+    - [x] test03: convert text data in resnpose msg
+    - [x] test04: create command to write mem area
     """
     def setUp(self):
         self.data_creator = DataCreator(
-                src_net_addr=1,
-                src_node_num=1,
+                src_net_addr=0,
+                src_node_num=170,
                 src_unit_addr=0,
                 srv_id=0)
     
@@ -38,7 +35,7 @@ class TestCase0(unittest.TestCase):
                 dst_net_addr=0,
                 dst_node_num=239,
                 dst_unit_addr=11)
-        self.assertEqual(header, b'80000200ef0b00020000')
+        self.assertEqual(header, b'\x80\x00\x02\x00\xef\x0b\x00\xaa\x00\x00')
     
     def test02(self):
         self.data_creator.set_destination(
@@ -47,64 +44,75 @@ class TestCase0(unittest.TestCase):
                 dst_unit_addr=11)
         data = self.data_creator.command_read_mem_area(
                 90, 0, 0, 1)
-        self.assertEqual(data, b'80000200ef0b0002000001015a0000000001')
+        self.assertEqual(data, b'\x80\x00\x02\x00\xef\x0b\x00\xaa\x00\x00\x01\x01Z\x00\x00\x00\x00\x01')
         
     def test03(self):
-        self.data_creator.set_destination(
-                dst_net_addr=0,
-                dst_node_num=239,
-                dst_unit_addr=11)
-        test_response = b'80000200ef0b0002000001015a5a29'
-        is_to_me = self.data_creator.is_response_to_me(test_response)
-        self.assertTrue(is_to_me)
+        test_response = b'\x80\x00\x02\x00\xef\x0b\x00\xaa\x00\x00\x01\x01\x00\x00\x00\x01'
+        fmt = [(DataCreator.BYTES, 12),
+               (DataCreator.USHORT, 2)]
+        data = self.data_creator.convert_ascii2data(
+                test_response, fmt)
+        self.assertEqual(data[1:], (0, 1))
 
     def test04(self):
-        test_response = b'80000200ef0b00020000010101010001'
-        fmt = [(DataCreator.BYTES, 10),
-               (DataCreator.USHORT, 3)]
-        data = self.data_creator._convert_ascii2data(
-                test_response, fmt)
-        self.assertEqual(data[1:], (257, 257, 1))
-
-    def test05(self):
         self.data_creator.set_destination(
                 dst_net_addr=0x0,
                 dst_node_num=0x2,
                 dst_unit_addr=0x12)
         data = self.data_creator.command_write_mem_area(
                 90, 0, 0, 1, (5, DataCreator.USHORT))
-        print(data)
+        self.assertEqual(
+                data, b'\x80\x00\x02\x00\x02\x12\x00\xaa\x00\x00\x01\x02Z\x00\x00\x00\x00\x01\x00\x05')
 
-    def test06(self):
-        ret = self.data_creator.open("192.168.2.13", 9600)
-        self.assertEqual(ret, 0)
+    def test05(self):
         self.data_creator.set_destination(
-                dst_net_addr=1,
-                dst_node_num=2,
+                dst_net_addr=0,
+                dst_node_num=13,
                 dst_unit_addr=0)
         comm = self.data_creator.command_read_mem_area(0xA0, 500, 0, 1)
-        print(comm)
-        data = self.data_creator.exec_read_mem_area(0xA0, 500, 0, 1)
-        print(data)
-        res = self.data_creator._convert_ascii2data(
-            data[1], [(DataCreator.CHAR, 14)])
-        print(res)
-        ret = self.data_creator.close()
-
-    def test07(self):
-        fins_instance = udp.UDPFinsConnection()
-        fins_instance.connect('192.168.2.13')
-        #i don't know what i had to write in this fields
-        fins_instance.dest_node_add= 0x01
-        #i don't know what i had to write in this fields
-        fins_instance.srce_node_add= 0x02
-         
-        mem_area = fins_instance.memory_area_read(b'\xA0')
-        print(mem_area)
+        self.assertEqual(
+                comm, b'\x80\x00\x02\x00\r\x00\x00\xaa\x00\x00\x01\x01\xa0\x01\xf4\x00\x00\x01')
 
     def tearDown(self):
         pass
 
+class TestCase1(unittest.TestCase):
+    """!
+    # Tester for FinsUDP.
+    This test can work with real PLC.
+    - [x] test01: reading words from a memory.
+    - [x] test02: writing word to a memory.
+    - [x] test03: reading bits from a memory.
+    """
+    def setUp(self):
+        self.fins = FinsUDP(0, 170)
+        ret = self.fins.open('192.168.2.13', 9600)
+        self.assertEqual(ret, 0)
+        self.fins.set_destination(
+                dst_net_addr=0,
+                dst_node_num=13,
+                dst_unit_addr=0)
+
+    def test01(self):
+        ret = self.fins.read_mem_area(0xA0, 500, 0, 4)
+        self.assertEqual(ret[0], 0)
+
+    def test02(self):
+        ret = self.fins.write_mem_area(
+                0xA0, 500, 0, 4, [(0, DataCreator.USHORT)]*4)
+        ret, value1 = self.fins.read_mem_area(0xA0, 500, 0, 4)
+        ret = self.fins.write_mem_area(
+                0xA0, 500, 0, 4, [(3, DataCreator.USHORT)]*4)
+        ret, value2 = self.fins.read_mem_area(0xA0, 500, 0, 4)
+        self.assertEqual(value2, (3, 3, 3, 3))
+        
+    def test03(self):
+        ret = self.fins.read_mem_area(0x20, 500, 0, 2)
+        print(ret)
+        self.assertEqual(ret[0], 0)
+
+    def tearDown(self):
+        self.fins.close()
 
 if __name__ == '__main__':
     unittest.main()
